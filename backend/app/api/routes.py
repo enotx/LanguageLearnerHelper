1593b141cd6a4d88eb4ca05f7080202b2be46139
enotx <enotxtone@gmail.com>
2025-04-25 03:01:23 +0800

    sadly, allosaurus does not work properly

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,14 +1,13 @@
 # backend/app/api/routes.py
 from fastapi import APIRouter, UploadFile, File #, WebSocket, WebSocketDisconnect, Depends, HTTPException
 import logging
-import json
-# from app.services.speech.xunfei import XunfeiSpeechService, convert_webm_to_pcm
-# from app.services.ai.risk_analyzer import RiskAnalyzerService
+# import json
+from app.services.audio_processing import AudioSegmentation
 from pydub import AudioSegment
 import io
 import tempfile
 import os
-import pickle
+# import pickle
 
 # 配置日志
 logging.basicConfig(
@@ -31,22 +30,25 @@ async def process_audio(audio: UploadFile = File(...)):
             temp_file.flush()
             
             # 使用 pydub 读取音频文件
-            audio_segment = AudioSegment.from_file(temp_file.name, format="webm")
+            # audio_segment = AudioSegment.from_file(temp_file.name, format="webm")
+            audio_segment = AudioSegmentation(temp_file.name)
+            # properties = audio_segment.getProperies()
+            ipa = audio_segment.recognize()
             
             # 获取音频属性
-            properties = {
-                "channels": audio_segment.channels,
-                "sample_width_bytes": audio_segment.sample_width,
-                "frame_rate_hz": audio_segment.frame_rate,
-                "duration_seconds": len(audio_segment) / 1000.0,
-                "frame_count": len(audio_segment.get_array_of_samples()),
-                "file_size_bytes": os.path.getsize(temp_file.name)
-            }
+            # properties = {
+            #     "channels": audio_segment.channels,
+            #     "sample_width_bytes": audio_segment.sample_width,
+            #     "frame_rate_hz": audio_segment.frame_rate,
+            #     "duration_seconds": len(audio_segment) / 1000.0,
+            #     "frame_count": len(audio_segment.get_array_of_samples()),
+            #     "file_size_bytes": os.path.getsize(temp_file.name)
+            # }
             
             # 删除临时文件
             os.unlink(temp_file.name)
             
-            return properties
+            return ipa
             
     except Exception as e:
         return {"error": f"Error processing audio: {str(e)}"}
--- backend/app/services/audio_processing.py
+++ backend/app/services/audio_processing.py
@@ -1,12 +1,37 @@
 from allosaurus.app import read_recognizer
+from pydub import AudioSegment
+import tempfile
+import os
 
 # model = read_recognizer()
 
-class AudioPart:
-    def __init__(self, start: float, end: float, text: str):
-        self.start = start
-        self.end = end
-        self.text = text
+class AudioSegmentation:
+    def __init__(self, File, audioformat="webm"):
+        self.audio_segment = AudioSegment.from_file(File, format=audioformat)
+        self.model = read_recognizer()
+        
 
-    def __repr__(self):
-        return f"AudioPart(start={self.start}, end={self.end}, text='{self.text}')"
+    def process(self, audio_file):
+        # 处理音频文件
+        result = self.model.transcribe(audio_file)
+        return result
+    
+    def recognize(self):
+        with tempfile.NamedTemporaryFile(delete_on_close=False, suffix='.wav') as fp:
+            self.audio_segment.export(fp
+                                      , format="wav"
+                                      , bitrate = "8k")
+            ipa = self.model.recognize(fp.name)
+        return ipa
+
+    def getProperies(self):
+        # 获取音频属性
+        properties = {
+            "channels": self.audio_segment.channels,
+            "sample_width_bytes": self.audio_segment.sample_width,
+            "frame_rate_hz": self.audio_segment.frame_rate,
+            "duration_seconds": len(self.audio_segment) / 1000.0,
+            "frame_count": len(self.audio_segment.get_array_of_samples()),
+            # "file_size_bytes": os.path.getsize(File)
+        }
+        return properties
